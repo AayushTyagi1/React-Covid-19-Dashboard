0daefb83025683223fd1e8cf88f43996d29c03a9
AayushTyagi1 <tyagiaayush5@gmail.com>
2020-11-30 17:25:48 +0530

    footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -31,6 +31,17 @@ class Home extends Component {
       <CountrySelector handleCountryChange={this.handleCountryChange}/>
       </div>
       <Charts data={data} country={country}/>
+      <footer>
+      <div className="App-footer">
+          <div className=" align-content offset-xs-0 offset-md-5 offset-lg-10 text-white-50">
+               Created By:<br/>
+              <a href="https://aayushtyagiresume.000webhostapp.com/" target="_blank">Aayush Tyagi</a><br/>
+              <a href="https://www.linkedin.com/in/aayush-tyagi-32a527190/" target="_blank">LinkedIn</a><br/>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTJFkzRjNxWClCGHDCxlTtDrwKXdKXDVwLHfxxpLdVhzrHmrQqkVjhMkpcQgzNcfMrqcBB" target="_blank">Mail</a>
+
+          </div>
+          </div>
+      </footer>
       </div>
     );
   }
