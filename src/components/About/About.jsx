5e08b218aa56fb7a9eeee7f4aa836142c41d178a
AayushTyagi1 <tyagiaayush5@gmail.com>
2020-11-30 17:26:56 +0530

    footer-about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -205,6 +205,17 @@ const About = () =>{
                 </div>
             </div>
         </div>
+        <footer>
+      <div className="App-footer">
+          <div className=" align-content offset-xs-0 offset-md-5 offset-lg-10 text-white-50">
+               Created By:<br/>
+              <a href="https://aayushtyagiresume.000webhostapp.com/" target="_blank">Aayush Tyagi</a><br/>
+              <a href="https://www.linkedin.com/in/aayush-tyagi-32a527190/" target="_blank">LinkedIn</a><br/>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTJFkzRjNxWClCGHDCxlTtDrwKXdKXDVwLHfxxpLdVhzrHmrQqkVjhMkpcQgzNcfMrqcBB" target="_blank">Mail</a>
+
+          </div>
+          </div>
+      </footer>
 
       </div>
         );
