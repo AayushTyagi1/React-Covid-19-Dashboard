bd8614cc1ca95faf277f0ed2aa807a32502d647a
AayushTyagi1 <tyagiaayush5@gmail.com>
2020-12-09 01:58:00 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React,{Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import {Home, About, GlobalMap, Navigation, Dark,Footer} from './components'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home, About, GlobalMap, Navigation, Dark, Footer } from "./components";
 
-import { TinyButton  } from "react-scroll-up-button";
+import { TinyButton } from "react-scroll-up-button";
 
 class App extends Component {
-
-  render(){
-    return(
-     
+  render() {
+    return (
       <BrowserRouter>
-      <div>
-      <Navigation />
-      </div> 
-      <div className="d-flex justify-content-end p-2">
-      <Dark />
-      </div>
-      <Switch>
-           <Route path="" component={Home} exact/>
-           <Route path="/about" component={About}/>
-           <Route path="/GlobalMap" component={GlobalMap}/>
-         </Switch>
-         <TinyButton />
-         <Footer/>
-    </BrowserRouter>
+        <div>
+          <Navigation />
+        </div>
+        <div className="d-flex justify-content-end p-2">
+          <Dark />
+        </div>
+        <Switch>
+          <Route exact href="/" component={Home} />
+          <Route href="/about" component={About} />
+          <Route href="/GlobalMap" component={GlobalMap} />
+        </Switch>
+        <TinyButton />
+        <Footer />
+      </BrowserRouter>
     );
   }
 }
--- src/components/GlobalMap/GlobalMap.jsx
+++ src/components/GlobalMap/GlobalMap.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import './GlobalMap.css';
-
+import React from "react";
+import "./GlobalMap.css";
 
 const GlobalMap = () => {
-    return(
-        <div>
-            <h1 className="globalhead">GLOBAL MAP</h1>
-            <div className="row align-content-center offset-1">
-            <div className="col-xs-12 col-sm-6 p-1">
-            <iframe src="https://www.google.com/maps/d/embed?mid=1a04iBi41DznkMaQRnICO40ktROfnMfMx" title="GlobalMap" width="580" height="480"></iframe>
-            </div>
-            
-            <div className="col-xs-12 col-sm-6 p-1">
-            <iframe src="https://public.domo.com/cards/dJ45D" title="GlobalTable" width="580" height="480"></iframe></div></div>
+  return (
+    <div>
+      <h1 className="globalhead">GLOBAL MAP</h1>
+      <div className="row align-content-center offset-1">
+        <div className="col-xs-12 col-sm-6 p-1">
+          <iframe
+            src="https://www.google.com/maps/d/embed?mid=1a04iBi41DznkMaQRnICO40ktROfnMfMx"
+            title="GlobalMap"
+            width="580"
+            height="480"
+          ></iframe>
         </div>
-    )
-}
-export default GlobalMap;
 
+        <div className="col-xs-12 col-sm-6 p-1">
+          <iframe
+            src="https://public.domo.com/cards/dJ45D"
+            title="GlobalTable"
+            width="580"
+            height="480"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default GlobalMap;
