612712a998cfaecc98b535be7decc01ea848ce2b
AayushTyagi1 <tyagiaayush5@gmail.com>
2020-12-09 08:45:28 +0530

    render-fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Home, About, GlobalMap, Navigation, Dark, Footer } from "./components";
 
 import { TinyButton } from "react-scroll-up-button";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      isUserAuthenticated: true,
+    };
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -14,10 +22,21 @@ class App extends Component {
         <div className="d-flex justify-content-end p-2">
           <Dark />
         </div>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return this.state.isUserAuthenticated ? (
+              <Redirect to="/" />
+            ) : (
+              <Redirect to="/404" />
+            );
+          }}
+        />
         <Switch>
-          <Route exact href="/" component={Home} />
-          <Route href="/about" component={About} />
-          <Route href="/GlobalMap" component={GlobalMap} />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/GlobalMap" component={GlobalMap} />
         </Switch>
         <TinyButton />
         <Footer />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import {Navbar, NavbarBrand, NavItem, Nav} from 'reactstrap';
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { Navbar, NavbarBrand, NavItem, Nav } from "reactstrap";
+import { NavLink } from "react-router-dom";
 import "./Navigation.css";
-import GithubCorner from '../GithubCorner/GithubCorner'
- 
+import GithubCorner from "../GithubCorner/GithubCorner";
+
 const Navigation = () => {
-    return (
-      <Navbar className="p-0 navbar">
-      <div  id="appcontainer">
-      <NavbarBrand className="p-2 navbar-light" href="/">COVID-19 Dashboard</NavbarBrand>
+  return (
+    <Navbar className="p-0 navbar">
+      <div id="appcontainer">
+        <NavbarBrand
+          className="p-2 navbar-light"
+          href="https://aayushtyagi1.github.io/COVID-19-Dashboard/"
+        >
+          COVID-19 Dashboard
+        </NavbarBrand>
       </div>
-          <Nav>
-        <NavItem className='p-2'>
-          <NavLink to="/" exact className="navbar-light">Home </NavLink></NavItem>
-          <NavItem className='p-2'><NavLink to="/GlobalMap" className="navbar-light">Map  </NavLink></NavItem>
-          <NavItem className='p-2'><NavLink to="/about" className="navbar-light">About</NavLink>
+      <Nav>
+        <NavItem className="p-2">
+          <NavLink to="/" exact className="navbar-light">
+            Home
+          </NavLink>
         </NavItem>
-              <GithubCorner/>
-          </Nav>
-       </Navbar>
-       
-    );
-}
+        <NavItem className="p-2">
+          <NavLink to="/GlobalMap" className="navbar-light">
+            Map
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-2">
+          <NavLink to="/about" className="navbar-light">
+            About
+          </NavLink>
+        </NavItem>
+        <GithubCorner />
+      </Nav>
+    </Navbar>
+  );
+};
 export default Navigation;
